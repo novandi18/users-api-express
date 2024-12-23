6ad2ab698d0c46eabcf713fabf25ac98c6a92a54
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 06:26:43 +0700

    feat: add route to get user by id

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { validatePagination, validateUser } from '../middlewares/validate.middleware';
-import { createUser, getAllUsers } from '../controllers/user.controller';
+import { createUser, getAllUsers, getUserById } from '../controllers/user.controller';
 
 const router = Router();
 
 router.get('/users', validatePagination, getAllUsers);
+router.get('/users/:id', getUserById);
 router.post('/users', validateUser, createUser);
 
 export default router;
