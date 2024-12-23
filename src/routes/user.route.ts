2fc1c901e16f728913658d8ad010d49c432093c3
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 06:36:55 +0700

    feat: add route to update user by id

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { validatePagination, validateUser } from '../middlewares/validate.middleware';
-import { createUser, getAllUsers, getUserById } from '../controllers/user.controller';
+import { createUser, getAllUsers, getUserById, updateUser } from '../controllers/user.controller';
 
 const router = Router();
 
 router.get('/users', validatePagination, getAllUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', validateUser, createUser);
+router.put('/users/:id', validateUser, updateUser);
 
 export default router;
