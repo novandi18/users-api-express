8bc6994a21aa38e2b0fc06eb9e9b88e416aabe09
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 05:12:05 +0700

    feat: add dotenv config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import userRoutes from './routes/user.route';
 import logger from './middlewares/logger.middleware';
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
