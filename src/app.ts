a22fb01079a8de4de0d84d2427533a711bfd5dc4
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 05:39:03 +0700

    refactor: separate server initialization from app configuration by moving app.listen to server.ts

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,4 @@ app.use(logger);
 
 app.use('/api', userRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
 export default app;
