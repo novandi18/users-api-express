63a7f6fd67e02a3afec1dad71a7377052db4224f
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 06:09:26 +0700

    feat: add user validation input for name, email and age

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -1,5 +1,35 @@
 import { Request, Response, NextFunction } from 'express';
 
+const validateUser = (req: Request, res: Response, next: NextFunction): void => {
+  const { name, email, age } = req.body;
+
+  if (typeof name !== 'string' || typeof email !== 'string' || typeof age !== 'number') {
+    res.status(400).json({
+      message: 'Invalid input',
+      success: false
+    });
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    res.status(400).json({
+      message: 'Invalid email format',
+      success: false
+    });
+  }
+  
+  const nameRegex = /^[A-Za-z\s]+$/;
+  if (!nameRegex.test(name) || name.length < 1) {
+    res.status(400).json({
+      message: 'Invalid name format',
+      success: false
+    });
+  }
+
+  next();
+};
+
+
 const validatePagination = (req: Request, res: Response, next: NextFunction): void => {
   const page = parseInt(req.query.page as string, 10) || 1;
   const limit = parseInt(req.query.limit as string, 10) || 10;
@@ -15,5 +45,6 @@ const validatePagination = (req: Request, res: Response, next: NextFunction): vo
 };
 
 export {
+  validateUser,
   validatePagination
 };
