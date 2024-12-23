dbb066485c97580b3ff5db3bf1e0eef85b65249c
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 06:11:28 +0700

    feat: add post route for create user with validation middleware

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { validatePagination } from '../middlewares/validate.middleware';
-import { getAllUsers } from '../controllers/user.controller';
+import { validatePagination, validateUser } from '../middlewares/validate.middleware';
+import { createUser, getAllUsers } from '../controllers/user.controller';
 
 const router = Router();
 
 router.get('/users', validatePagination, getAllUsers);
+router.post('/users', validateUser, createUser);
 
 export default router;
