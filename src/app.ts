a80cd0d8bc777f452f899bf2cebee030169433d6
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 07:17:53 +0700

    feat: add swagger UI documentation route at /docs

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import dotenv from 'dotenv';
 import express from 'express';
 import userRoutes from './routes/user.route';
 import logger from './middlewares/logger.middleware';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocument } from './docs/swagger.docs';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(logger);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api', userRoutes);
 
