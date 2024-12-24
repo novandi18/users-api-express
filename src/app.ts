92f7947a1a0022e1b82b69e22221d94279b5f53e
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 08:56:25 +0700

    feat: add body-parser middleware for json and url

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,18 @@ import userRoutes from './routes/user.route';
 import logger from './middlewares/logger.middleware';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerDocument } from './docs/swagger.docs';
+import bodyParser = require('body-parser');
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(logger);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
