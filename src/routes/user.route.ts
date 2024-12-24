c3e5af225cf35c34eb9462a223bdb38f51bca1b5
Novandi Ramadhan <novandiramadhan80@gmail.com>
2024-12-24 07:03:27 +0700

    feat: add route for delete user by id

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { validatePagination, validateUser } from '../middlewares/validate.middleware';
-import { createUser, getAllUsers, getUserById, updateUser } from '../controllers/user.controller';
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/user.controller';
 
 const router = Router();
 
@@ -8,5 +8,6 @@ router.get('/users', validatePagination, getAllUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', validateUser, createUser);
 router.put('/users/:id', validateUser, updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
